011152a17d9a2a2e8da845e1e8cc5950e039a2e4
van tuan <huynhvantuan889@gmail.com>
2022-12-08 11:47:55 +0700

    up

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,19 +6,21 @@ import Header from "../../components/Header/Header";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
+  const user = JSON.parse(window.localStorage.getItem("user"));
   useEffect(() => {
     const res = async () => {
       try {
         await appApi.checkDisableUser();
       } catch (error) {
-        const user = JSON.parse(window.localStorage.getItem("user"));
         await authApi.logout(user._id);
         window.localStorage.removeItem("user");
         navigate("/expired");
       }
     };
-    res();
-  }, []);
+    if (user) {
+      res();
+    }
+  }, [user]);
   return (
     <>
       <Header />
